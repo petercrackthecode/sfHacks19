f6b0928e1259bc117be813d33791798aef0553c7
ductai199x <taiducnguyen.drexel@gmail.com>
2018-07-31 13:47:28 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+=======
+import { BrowserRouter } from 'react-router-dom';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render((
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+), document.getElementById('root'));
+>>>>>>> second commit
 registerServiceWorker();
